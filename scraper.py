3153c6d51c76acb423a57faf0db2a46aced38e72
yuuikm <morozov-mark@bk.ru>
2026-03-18 11:25:03 +0500

    fix: changed gitignore

--- scraper.py
+++ scraper.py
@@ -30,7 +30,7 @@ def get_dynamic_html(url):
     options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")  # Added for stability in containers
+    options.add_argument("--disable-dev-shm-usage") 
     
     chromedriver_path = os.getenv("CHROMEDRIVER_PATH", "/usr/bin/chromedriver")
     service = Service(chromedriver_path)
